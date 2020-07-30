cbaf0c66e2b6efd86ca320ee87fba054176fbbb4
Jose Molinos <molinossobrino.jose@gmail.com>
2020-07-30 18:31:33 +0200

    Utilidad de Interfaces

--- src/com/company/Aplicacion.java
+++ src/com/company/Aplicacion.java
@@ -1,10 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Aplicacion {//antes era main (se cambio el nombre)
 
     public static void main(String[] args) {
 	// write your code here
-        JuegoAdivinaNumero juego=new JuegoAdivinaNumero(5,7);
+        Scanner leer=new Scanner(System.in);
+        String seguir;
+        do {
+            Jugable juego = EligeJuego();
+            juego.MuestraNombre();
+            juego.MuestraInfo();
+            juego.Juega();
+            System.out.println("Seguir jugando s/n \n");
+            seguir=leer.nextLine();
+        }while (seguir.equals("s"));
+
+      /*  JuegoAdivinaNumero juego=new JuegoAdivinaNumero(5,7);
         JuegoAdivinaPar juego2=new JuegoAdivinaPar(3,4);
         JuegoAdivinaImpar juego3=new JuegoAdivinaImpar(3,5);
         juego.MuestraNombre();
@@ -15,8 +28,29 @@ public class Aplicacion {//antes era main (se cambio el nombre)
         juego2.Juega();
         juego3.MuestraNombre();
         juego3.MuestraInfo();
-        juego3.Juega();
+        juego3.Juega();*/
 
+    }
+
+    public static Jugable EligeJuego(){
+        JuegoAdivinaNumero juego1=new JuegoAdivinaNumero(5,7);
+        JuegoAdivinaPar juego2=new JuegoAdivinaPar(3,4);
+        JuegoAdivinaImpar juego3=new JuegoAdivinaImpar(3,5);
+        Jugable [] juegos=new Jugable[3];
+        juegos[0]=juego1;
+        juegos[1]=juego2;
+        juegos[2]=juego3;
+        Scanner leer=new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("Selecciona un Juego\n" +
+                    "0. Juego Adivina un numero\n" +
+                    "1. Juego Adivina un numero Par\n" +
+                    "2. Juego Adivina un numero Impar\n");
+            opcion=leer.nextInt();
+            leer.nextLine();
+        }while(!(opcion<=2 & opcion>=0));
+        return juegos[opcion];
     }
 }
 
